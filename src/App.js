f41eeb630b802f4f2743d04b1a88ceb843d73570
Jangwoong Kim <6812skiii@gmail.com>
2021-01-24 01:35:50 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Submit your query.
+	  <form>
+            <label>
+              Query: 
+                <input type="text" name="query" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
